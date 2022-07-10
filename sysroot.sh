8024a4355d516c7e68da1a4359dc3eb8bfde3a37
Mariotaku <mariotaku.lee@gmail.com>
2022-07-10 19:45:55 +0900

    packages file support

--- sysroot.sh
+++ sysroot.sh
@@ -4,6 +4,8 @@ if [ -z "$PACKAGES" ]; then
   echo "Following environment variables are required:"
   echo "PACKAGES: Packages list needs to be installed"
   exit 1
+elif [ -f "$PACKAGES" ]; then
+  PACKAGES=$(cat "$PACKAGES")
 fi
 
 # Install dependencies
