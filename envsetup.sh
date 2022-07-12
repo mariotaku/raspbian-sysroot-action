3533647b5c9c5e9efb22d85279264ebb9c97d961
Mariotaku <mariotaku.lee@gmail.com>
2022-07-13 00:28:31 +0900

    added script for autoconf

---
+++ envsetup.sh
@@ -0,0 +1,8 @@
+export SYSROOT="@SYSROOT@"
+
+export CFLAGS="--sysroot=${SYSROOT}"
+export CXXFLAGS="--sysroot=${SYSROOT}"
+export LDFLAGS="--sysroot=${SYSROOT} -L${SYSROOT}/usr/lib"
+
+export PKG_CONFIG_SYSROOT_DIR="${SYSROOT}"
+export PKG_CONFIG_LIBDIR="${SYSROOT}/usr/lib/arm-linux-gnueabihf/pkgconfig:${SYSROOT}/usr/lib/pkgconfig:${SYSROOT}/usr/share/pkgconfig:${SYSROOT}/opt/vc/lib/pkgconfig"
